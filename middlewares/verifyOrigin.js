88093b578e280568c330d828e174d937621cac70
cristhiam <jaracristhiam@gmail.com>
2022-07-18 21:30:10 -0500

    log origin

--- middlewares/verifyOrigin.js
+++ middlewares/verifyOrigin.js
@@ -1,6 +1,7 @@
 const keys = require("../keys/keys");
 
 function verifyOrigin(req,res,next){
+  console.log({origin:req.headers.origin});
   if(req.headers.origin!==keys.corsOrigin||!req.headers.origin){
     return res.send({mensaje:"usted no esta autorizado"});
   }else{
